aa3d85585f3115840c9074138f5ec87b425cba73
jeroen@svobo.nl <**western^BELFAST^thin^38>
2022-11-24 14:22:17 +0100

    test

---
+++ README.md
@@ -0,0 +1,38 @@
+# Internet radio
+## PROJECT
+| Project name:    | Halflink/InternetRadio |
+|------------------|------------------------|
+| Author:          | Jeroen van Zwam        |
+| Date:            | 2022-11-24             |  
+| Last update: | 2022-11-24             |
+| Project type:    | Raspberry Pi           |
+***
+
+## Executable
+* Start  
+
+## PROJECT DESCRIPTION
+
+### Scope
+- Use VLC to play internetstream
+- power button 
+- LED strip:
+  - 
+
+### Pi 3 Model B choice
+
+### HIFIBerry AP2
+
+## PI INSTALLATION NOTES
+* Connect remotely so we can use the raspberry headless
+  * Set up SSH 
+  * Set up WIFI
+  * Install GIT `sudo apt-get install git`
+* HIFIBerry  
+
+## PARTS LIST*
+* 1x Raspberry Pi 3 Model B
+
+## TO DO LIST
+
+## Progress
---
+++ radio.py
@@ -0,0 +1,41 @@
+# RADIO class
+#
+# Requisites:
+# sudo pip install VNC
+#
+# tested with:
+# http://playerservices.streamtheworld.com/api/livestream-redirect/KINK.mp3
+class Radio:
+
+    import time
+    import vnc
+
+    def __init__(self):
+
+        # initiate variables
+        self.volume = 20
+        self.current_url = ""
+
+        # creating a vlc instance
+        self.vlc_instance = vlc.Instance()
+        self.player = self.vlc_instance.media_player_new()
+
+    def set_media(self, source):
+        # creating a media
+        media = self.vlc_instance.media_new(source)
+
+        # setting media to the player
+        self.player.set_media(media)
+
+    def play_media(self):
+        # play the audio
+        self.player.play()
+
+        # wait time
+        self.time.sleep(100)
+
+
+if __name__ == '__main__':
+    radio = Radio()
+    radio.set_media('http://playerservices.streamtheworld.com/api/livestream-redirect/KINK.mp3')
+    radio.play_media()
