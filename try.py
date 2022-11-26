fa9011b0c70267e0af2c7c1de8e6f9bc7b032ded
jeroen@svobo.nl <**western^BELFAST^thin^38>
2022-11-26 15:42:38 +0100

    test

--- try.py
+++ try.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python
 
-import Adafruit_CharLCD as LCD
+import board
+import digitalio
+
+
+
+# import Adafruit_CharLCD as LCD
 
 # Raspberry Pi pin configuration:
-lcd_rs = 25
-lcd_en = 24
-lcd_d4 = 23
-lcd_d5 = 17
-lcd_d6 = 18
-lcd_d7 = 22
-lcd_backlight = 1
+# lcd_rs = 25
+# lcd_en = 24
+# lcd_d4 = 23
+# lcd_d5 = 17
+# lcd_d6 = 18
+# lcd_d7 = 22
+# lcd_backlight = 1
 
 # Define LCD column and row size for 16x2 LCD.
-lcd_columns = 16
-lcd_rows    = 2
+# lcd_columns = 16
+# lcd_rows    = 2
 
 # Initialize the LCD using the pins above.
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+# lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
                            lcd_columns, lcd_rows, lcd_backlight)
 
 # Print a two line message
-print('probeer ')
-lcd.message('Hello!\nraspberrytips.nl')
+# print('probeer ')
+# lcd.message('Hello!\nraspberrytips.nl')
+
+lcd_rs = digitalio.DigitalInOut(board.D25)
+lcd_en = digitalio.DigitalInOut(board.D24)
+lcd_d4 = digitalio.DigitalInOut(board.D23)
+lcd_d5 = digitalio.DigitalInOut(board.D17)
+lcd_d6 = digitalio.DigitalInOut(board.D18)
+lcd_d7 = digitalio.DigitalInOut(board.D22)
+
+lcd_columns = 16
+lcd_rows = 2
+
+lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
+
+print('prpbeer')
+lcd.message = "Hello\nCircuitPython"
