66e60d064874610c33f15a47b27f569ddd359842
jeroen@svobo.nl <**western^BELFAST^thin^38>
2022-11-26 15:43:28 +0100

    test

--- try.py
+++ try.py
@@ -22,7 +22,7 @@ import digitalio
 
 # Initialize the LCD using the pins above.
 # lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-                           lcd_columns, lcd_rows, lcd_backlight)
+#                           lcd_columns, lcd_rows, lcd_backlight)
 
 # Print a two line message
 # print('probeer ')
