848010589f5819f6e818f85f10fb448d7e6023da
jeroen@svobo.nl <**western^BELFAST^thin^38>
2022-11-26 15:45:18 +0100

    test

--- try.py
+++ try.py
@@ -38,7 +38,7 @@ lcd_d7 = digitalio.DigitalInOut(board.D22)
 lcd_columns = 16
 lcd_rows = 2
 
-lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
 
 
 print('prpbeer')
