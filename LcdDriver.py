0d9003fac586e49b0275be97bcb456581415dffb
jeroen@svobo.nl <**western^BELFAST^thin^38>
2022-12-01 16:54:34 +0100

    test

--- LcdDriver.py
+++ LcdDriver.py
@@ -1,81 +1,83 @@
 #!/usr/bin/python
 
-import smbus
-import time
-import datetime
+class LcdDriver:
+  import smbus
+  import time
+  import datetime
 
-I2C_ADDR  = 0x27 # I2C device address
-LCD_WIDTH = 16   # Maximum characters per line
+  I2C_ADDR  = 0x27 # I2C device address
+  LCD_WIDTH = 16   # Maximum characters per line
 
-# Define some device constants
-LCD_CHR = 1 # Mode - Sending data
-LCD_CMD = 0 # Mode - Sending command
+  # Define some device constants
+  LCD_CHR = 1 # Mode - Sending data
+  LCD_CMD = 0 # Mode - Sending command
 
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+  LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+  LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
 
-LCD_BACKLIGHT  = 0x08  # On 0X08 / Off 0x00
+  LCD_BACKLIGHT  = 0x08  # On 0X08 / Off 0x00
 
-ENABLE = 0b00000100 # Enable bit
+  ENABLE = 0b00000100 # Enable bit
 
-E_PULSE = 0.0005
-E_DELAY = 0.0005
+  E_PULSE = 0.0005
+  E_DELAY = 0.0005
 
-bus = smbus.SMBus(1) # Rev 2 Pi uses 1
+  bus = smbus.SMBus(1) # Rev 2 Pi uses 1
 
-def lcd_init():
-  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
-  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
-  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
-  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
-  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
-  time.sleep(E_DELAY)
+  def lcd_init():
+    lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+    lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+    lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+    lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+    lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+    lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+    time.sleep(E_DELAY)
 
-def lcd_byte(bits, mode):
+  def lcd_byte(bits, mode):
 
-  bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
-  bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
+    bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
+    bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
 
-  bus.write_byte(I2C_ADDR, bits_high)
-  lcd_toggle_enable(bits_high)
+    bus.write_byte(I2C_ADDR, bits_high)
+    lcd_toggle_enable(bits_high)
 
-  bus.write_byte(I2C_ADDR, bits_low)
-  lcd_toggle_enable(bits_low)
+    bus.write_byte(I2C_ADDR, bits_low)
+    lcd_toggle_enable(bits_low)
 
-def lcd_toggle_enable(bits):
-  time.sleep(E_DELAY)
-  bus.write_byte(I2C_ADDR, (bits | ENABLE))
-  time.sleep(E_PULSE)
-  bus.write_byte(I2C_ADDR,(bits & ~ENABLE))
-  time.sleep(E_DELAY)
+  def lcd_toggle_enable(bits):
+    time.sleep(E_DELAY)
+    bus.write_byte(I2C_ADDR, (bits | ENABLE))
+    time.sleep(E_PULSE)
+    bus.write_byte(I2C_ADDR,(bits & ~ENABLE))
+    time.sleep(E_DELAY)
 
-def lcd_string(message,line):
+  def lcd_string(message,line):
 
-  message = message.ljust(LCD_WIDTH," ")
+    message = message.ljust(LCD_WIDTH," ")
 
-  lcd_byte(line, LCD_CMD)
+    lcd_byte(line, LCD_CMD)
 
-  for i in range(LCD_WIDTH):
-    lcd_byte(ord(message[i]),LCD_CHR)
+    for i in range(LCD_WIDTH):
+      lcd_byte(ord(message[i]),LCD_CHR)
 
-def main():
+  def main():
 
-  lcd_init()
+    lcd_init()
 
-  while True:
+    while True:
 
-    now = datetime.datetime.now()
+      now = datetime.datetime.now()
 
-    lcd_string("RASPBERRYTIPS.NL",LCD_LINE_1)
-    lcd_string( str(now.hour) + ":" + str(now.minute) ,LCD_LINE_2)
-    time.sleep(1)
+      lcd_string("RASPBERRYTIPS.NL",LCD_LINE_1)
+      lcd_string( str(now.hour) + ":" + str(now.minute) ,LCD_LINE_2)
+      time.sleep(1)
 
 if __name__ == '__main__':
 
+  lcd = LcdDriver()
   try:
-    main()
+    lcd.main()
   except KeyboardInterrupt:
     pass
   finally:
-    lcd_byte(0x01, LCD_CMD)
+    lcd.lcd_byte(0x01, LCD_CMD)
