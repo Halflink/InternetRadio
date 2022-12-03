d7cd310511d1415cc00f6295c42f892b5b386e74
jeroen@svobo.nl <**western^BELFAST^thin^38>
2022-12-03 15:19:17 +0100

    test

--- LcdMessageHandler.py
+++ LcdMessageHandler.py
@@ -7,8 +7,8 @@ class LcdMessageHandler:
     def __init__(self):
         self.lcd_width = 16
         self.Lcd = self.LcdDriver(lcd_address=0x27, lcd_with=self.lcd_width, lcd_lines=2)
-        self.message_start = 15
-        self.message_end = 15
+        self.message_start = 0
+        self.message_end = 0
         self.current_message = ''
 
     def clean_up(self):
@@ -18,20 +18,16 @@ class LcdMessageHandler:
         self.Lcd.lcd_string(self.get_datetime(), self.Lcd.LCD_LINE_ADDRESS[0])
 
     def display_news_ticker(self):
-        if len(self.current_message) >= 16:
-            display_message = self.current_message[self.message_start:self.message_end].rjust(self.lcd_width, ' ')
-            print(display_message)
-            # set new range to display
-            if self.message_start == 0:
-                if self.message_end == 0:
-                    self.message_start = 15
-                    self.message_end = 15
-                else:
-                    self.message_end == self.message_end - 1
-            else:
-                self.message_start = self.message_start - 1
 
-            self.Lcd.lcd_string(display_message, self.Lcd.LCD_LINE_ADDRESS[1])
+        display_message = self.current_message[self.message_start:self.message_end].rjust(self.lcd_width, ' ')
+        print(display_message)
+        # set new range to display
+        if self.message_end == len(self.current_message) - 1:
+            self.message_end = self.message_end + 1
+            if self.message_start < self.message_end - 15:
+                self.message_start = self.message_end - 15
+
+        self.Lcd.lcd_string(display_message, self.Lcd.LCD_LINE_ADDRESS[1])
 
     def get_datetime(self):
         # see https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
@@ -40,7 +36,7 @@ class LcdMessageHandler:
         return time_string
 
     def set_current_message(self, message):
-        self.current_message = message.ljust(self.lcd_width, ' ')
+        self.current_message = message
 
 
 if __name__ == '__main__':
