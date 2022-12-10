6d0539d52c6cf9064ee887c4dbd85f4eb508ae29
jeroen@svobo.nl <**western^BELFAST^thin^38>
2022-12-10 15:08:28 +0100

    test

--- radio.py
+++ radio.py
@@ -86,7 +86,7 @@ class Radio:
 
 if __name__ == '__main__':
     radio = Radio()
-    radio.play_media('http://playerservices.streamtheworld.com/pls/KINK_DNA.pls')
+    radio.play_media('https://playerservices.streamtheworld.com/api/livestream-redirect/KINK_DISTORTION.mp3 ')
     while True:
         pass
 
